3c917aabe9c678f2d4b999b12f2956726fb3320e
matheusanjos200203 <matheusanjos200203@gmail.com>
2022-09-11 14:48:24 -0300

    Scroll ativado para o overflow da página

--- financeiro/src/Components/HomePage/Login/index.jsx
+++ financeiro/src/Components/HomePage/Login/index.jsx
@@ -4,27 +4,27 @@ import Button from '../Buttons/Button';
 
 const Login = () => {
     return(
-        <section className="Login">
-        {/* <img src={HomeMobile} alt="Análise Métrica Finanças" className="ImgMobile" /> */}
-        <h1>
-            BEM-VINDO AO FINC3IRO
-        </h1>
+        // <UserProvider>
+            <section className="Login">
+                <h1>
+                    BEM-VINDO AO FINC3IRO
+                </h1>
 
-        <p>Acesse sua conta agora mesmo!</p>
+                <p>Acesse sua conta agora mesmo!</p>
 
-        <form action="submit" className="formHome" id="formLogin">
-            <fieldset className="campoLogin">
-                <label htmlFor="emailLogin" className="labelLogin">Email</label>
-                <input type="email" name="emailLogin" placeholder="Email" required className="inputLogin" autoComplete="userName"/>
-                <label htmlFor="senha" className="labelLogin">Senha</label>
-                <input type="password" name="senhaLogin" id="senhaLogin" placeholder="Senha" required className="inputLogin" autoComplete="current-password"/>
-            </fieldset>
-            
-            <Button type='submit' className='formButton loginButton'>Entrar</Button>
-            <LinkButton className = "togglePage" >Cadastre-se</LinkButton>
-        </form>
-    </section>
-
+                <form action="submit" className="formHome" id="formLogin">
+                    <fieldset className="campoLogin">
+                        <label htmlFor="emailLogin" className="labelLogin">Email</label>
+                        <input type="email" name="emailLogin" placeholder="Email" required className="inputLogin" autoComplete="userName"/>
+                        <label htmlFor="senha" className="labelLogin">Senha</label>
+                        <input type="password" name="senhaLogin" id="senhaLogin" placeholder="Senha" required className="inputLogin" autoComplete="current-password"/>
+                    </fieldset>
+                    
+                    <Button type='submit' className='formButton loginButton'>Entrar</Button>
+                    <LinkButton className = "togglePage" >Cadastre-se</LinkButton>
+                </form>
+            </section>
+        // {/* </UserProvider> */}
     );
 }
 
